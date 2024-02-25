4846e478018335ce156f3daa1b008feebfa1c934
Artem <Artem93.00yandex.ru>
2024-02-25 17:24:31 +0300

    первая фиксация

---
+++ 3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число");
+int N = int.Parse(Console.ReadLine());
+int N2 = -N;
+for (int i = N2; i <= N; i++)
+{
+    Console.Write (i+" ");
+}
---
+++ Program1.cs
@@ -0,0 +1,23 @@
+﻿/*
+Console.WriteLine("Введите квадрат числа");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
+*/
+
+Console.Write("Введите число");
+/*int N = int.Parse(Console.ReadLine());
+int N2 = -N;
+for (int i = N2; i <= N; i++)
+{
+    Console.Write (i+" ");
+}
+*/
